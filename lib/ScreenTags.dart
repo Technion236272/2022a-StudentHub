d112cd80ce42d173821c1c0bfd21836e5427861f
mostafanaax69 <mostafanaa@campus.technion.ac.il>
2021-12-22 18:37:07 +0200

    New fonts/colors/images

--- lib/ScreenTags.dart
+++ lib/ScreenTags.dart
@@ -29,6 +29,7 @@ class GlobalStringText {
 
   /////////////////////////colors/////////////////////////
   static Color whiteColor =  Color(0xfff9f9f9);
+  static Color WhiteScreen = Color(0xffFFFFFF);
   static Color textFieldColor = Color(0xfff0f4f8);
   static Color textFieldPinkColor = Color(0xfff8e4e4);
   static Color textFieldDeepPinkColor = Color(0xffF8C4C4);
@@ -39,11 +40,12 @@ class GlobalStringText {
   static Color LightOarngeColorFirst = Color(0xffFFD387);
   static Color LightRedColorFirst = Color(0xffFF8787);
   static Color textFieldGrayColor = Color(0xffA6BCD0);
-  static Color FirstpurpleColor =  Color(0xffA1A3FF);
+  static Color FirstpurpleColor =  Color(0xff6F65EE);
   static Color SecondpurpleColor =  Color(0xff7B74F3);
   static Color ThirdpurpleColor =  Color(0xff756CF1);
   static Color ForthpurpleColor =  Color(0xff7268F0);
-  static Color FifthpurpleColor =  Color(0xff6D63EF);
+  static Color FifthpurpleColor =  Color(0xff6D63EA);
+
   static Color WhiteColorHiMessage =  Color(0xffF5F5F5);
 
 
@@ -59,11 +61,16 @@ class GlobalStringText {
   static String ImagesEnertiamentCat = "images/entCat.png";
   static String ImagesStudyBudCat = "images/studdybudCat.png";
   static String ImagesTravelCat = "images/TravelCat.png";
+  static String ImageWaving = "images/waving-hand-emoji-svg-11549875522a4ojenddj3.png";
+  static String ImageWavingTest = "images/32px-Twemoji12_1f44b.svg.png";
+
 
   static String ImagesFoodCat = "images/foodcat.png";
   static String ImagesAcadSupportCat = "images/acaCat.png";
-
   static String ImagesMaterialCat = "images/matCatogry.png";
+  static String ImagesServices = "images/services.png";
+  static String ImagesTickets = "images/tickets.png";
+  static String ImagesProfile = "images/profile.png";
 
 
 
@@ -72,7 +79,7 @@ class GlobalStringText {
   static String ImagesTravel = "images/icons8-car-64.png";
   static String ImagesEntertiament = "images/icons8-comedy-64.png";
   static String ImagesFood = "images/food.png";
-  static String ImagesServices = "assets/images/icons8-service-64.png";
+
   static String ImagesAcadimicSupport = "assets/images/icons8-helping-hand-64.png";
   static String ImagesStudyBuddy = "assets/images/icons8-book-and-pencil-64.png";
 
