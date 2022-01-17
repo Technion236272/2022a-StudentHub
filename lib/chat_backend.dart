e2c0e4fd1dea17bf366c8dd11acee9d9277653c8
Raja Zidane <rzidane68@gmail.com>
2022-01-17 20:26:02 +0200

    issues #50 #51 #52

--- lib/chat_backend.dart
+++ lib/chat_backend.dart
@@ -1,17 +1,21 @@
 
 
 
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
+import 'package:studenthub/inboxScreen.dart';
 
 import 'Auth.dart';
 
 class Chat {
   static List chats = [];
+  static List groups = [];
   static List messages = [];
   static late User? user;
   static final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -20,56 +24,57 @@ class Chat {
     user = Provider.of<AuthRepository>(context, listen: false).user;
   }
 
-  static Future<void> send(String message, String receiverUid, {String? name}) async {
-    final ref = await firestore.collection('messages').add({
-      'sender' : user!.uid,
+  static void send(String message, String receiverUid, String name) {
+    final timeStamp = Timestamp.now();
+    var ids = [user!.uid, receiverUid];
+    ids.sort();
+    firestore.collection('chats/${ids[0]}-${ids[1]}/messages').add({
+      'senderId' : user!.uid,
+      'senderName' : user!.displayName,
       'message' : message,
-      'timeStamp' : Timestamp.now()
+      'timeStamp' : timeStamp,
     });
-    if(name != null) {
-      firestore.collection('${user!.uid} chats').doc(receiverUid).set({'name': name});
-      firestore.collection('${receiverUid} chats').doc(user!.uid).set({'name': user!.displayName});
-    }
-    firestore.collection('${user!.uid} chats').doc(receiverUid).collection(name!).add({
-      'ref' : ref
-    });
-    firestore.collection('${receiverUid} chats').doc(user!.uid).collection(user!.displayName!).add({
-      'ref' : ref
-    });
+    firestore.collection('users/${user!.uid}/chats').doc(receiverUid).set({'name': name, 'lastMessage' : message, 'time' : timeStamp, 'isRead' : true});
+    firestore.collection('users/$receiverUid/chats').doc(user!.uid).set({'name': user!.displayName, 'lastMessage' : message, 'time' : timeStamp, 'isRead' : false});
   }
 
-  static Future<void> getChats() async {
-    if (user == null) {
-      //do something
-      return;
-    }
-    chats = [];
-    await firestore.collection('${user!.uid} chats').get().then((value) {
-      var i = 0;
-      for (var element in value.docs) {
-        i++;
-        chats.add({
-          'uid' : element.id,
-          'name' : element.data()['name'],
-          'messages' : element.reference.collection('message refs')
-        });
-      }
-    });
+  static Stream<QuerySnapshot<Map<String,dynamic>>> getChats()  {
+    return firestore.collection('users/${user!.uid}/chats').orderBy('time').snapshots();
   }
 
 
-  static Future<void> getMessages(CollectionReference collection) async {
-    messages = [];
-    await collection.get().then((value) async {
-      for (var element in value.docs) {
-        Map<String, dynamic> ref = element.data() as Map<String, dynamic>;
-        await ref['ref'].get().then((value) {
-          messages.add(value.data() as Map<String, dynamic>);
-        });
-      }
-    }).then((value) {
-    });
+  static Stream<QuerySnapshot<Map<String,dynamic>>> getMessages(String uid) {
+    var ids = [user!.uid, uid];
+    ids.sort();
+    firestore.collection('users/${user!.uid}/chats').doc(uid).set({'isRead' : true});
+    return firestore.collection('chats/${ids[0]}-${ids[1]}/messages').orderBy('timeStamp', descending: true).snapshots();
+  }
 
+  static void sendGroup(String message, String groupId) {
+    final timeStamp = Timestamp.now();
+    firestore.collection('chats/$groupId/messages').add({
+      'senderId' : user!.uid,
+      'senderName' : user!.displayName,
+      'message' : message,
+      'timeStamp' : timeStamp,
+    });
+    firestore.collection('chats').doc(groupId).set({'subs' : FieldValue.arrayUnion([user!.uid])}, SetOptions(merge: true));
+    firestore.collection('chats').doc(groupId).get().then((value) {
+      List<dynamic> subs = value.data()?['subs'];
+      for (var element in subs) {
+        firestore.collection('users/${element as String}/groups').doc(groupId).set({'lastMessage' : message, 'time' : timeStamp, 'isRead' : false});
+      }
+    });
+    firestore.collection('users/${user!.uid}/groups').doc(groupId).set({'isRead' : true});
+  }
+
+  static Stream<QuerySnapshot<Map<String,dynamic>>> getGroupChats()  {
+    return firestore.collection('users/${user!.uid}/groups').orderBy('time').snapshots();
+  }
+
+
+  static Stream<QuerySnapshot<Map<String,dynamic>>> getGroupMessages(String groupId) {
+    return firestore.collection('chats/$groupId/messages').snapshots();
   }
 
 }
